e3b4a55e54ea33c7412885565ce4c560de5e71f6
Kaung Myat Thu <kaungmyatthu692017@gmail.com>
2023-06-12 00:36:27 +0630

    finished

---
+++ index.js
@@ -0,0 +1,37 @@
+
+const { log } = require("debug/src/browser");
+let express = require("express");
+let socket = require("socket.io");
+
+
+
+/* app setup */
+
+let app = express();
+
+/* server setup */
+
+let server = app.listen(4000, () => {
+      console.log("project is running on localhost:4000");
+    });
+
+/** Rotue setup */
+app.get('/', (res, req) => {
+        req.sendFile(__dirname + '/public/index.html');
+    })
+
+
+/** Socket setup */
+ 
+let io = socket(server);
+ io.on('connection', (socket) => {
+    socket.on('chat', (data)=>{
+       io.sockets.emit('chat', data);
+    }),
+    socket.on('name', (name)=>{
+           socket.broadcast.emit('typing', name);
+        })
+
+ })
+
+
